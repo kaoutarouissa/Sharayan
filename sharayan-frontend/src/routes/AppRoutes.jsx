c5868219dd20520a2b2ca1963561fc4c464b4fcd
kaoutar <hodabz091@gmail.com~>
2026-09-09 16:56:16 +0100

    feat(auth): ajout de la page d'inscription et configuration des routes

--- sharayan-frontend/src/routes/AppRoutes.jsx
+++ sharayan-frontend/src/routes/AppRoutes.jsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "../pages/Login";
 import Home from "../pages/Home";
+import Register from "../pages/Register"
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/Register" element={<Register/>}></Route>
     </Routes>
   );
 }

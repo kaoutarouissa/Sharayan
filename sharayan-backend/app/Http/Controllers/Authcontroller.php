05ee4813d4a23a84a2dbc800dff8e88487b2abdf
kaoutar <hodabz091@gmail.com~>
2026-09-10 13:01:12 +0100

    Ajout de l'authentification login avec token

--- sharayan-backend/app/Http/Controllers/Authcontroller.php
+++ sharayan-backend/app/Http/Controllers/Authcontroller.php
@@ -5,36 +5,56 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+// use Laravel\Sanctum\HasApiTokens;
+use Laravel\Sanctum\HasApiTokens;
 
 class Authcontroller extends Controller
 {
     //
-    public function login()
+    use HasApiTokens;
+    public function login(Request $request)
     {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+
+        ]);
+        $user = User::where('email', $request->email)->first();
+        if (!$user || !Hash::check($request->password, $user->password)) {
+            return response()->json([
+                'message' => 'Email ou mot de passe incorrect',
+            ], 401);
+        }
+        $token = $user->createToken('auth_token')->plainTextToken;
+        return response()->json([
+            'message' => 'Login réussi',
+            'token' => $token,
+            'user' => $user,
+        ], 200);
 
     }
 
     public function register(Request $request)
     {
         $request->validate([
-            'name'=>'required|string',
-            'telephone'=>'required|string',
-            'email'=>'required|email|unique:users,email',
-            'password'=>'required|string|min:7|confirmed',
-            'role'=>'required|in:admin,patient,donneur',          'role'=>'required|in:admin,donneur,patient',
+            'name' => 'required|string',
+            'telephone' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:7|confirmed',
+            'role' => 'required|in:admin,patient,donneur',
         ]);
-         $user=User::create([
-            'name'=>$request->name,
-            'email'=>$request->email,
-            'password'=>Hash::make($request->password),
-            'telephone'=>$request->telephone,
-            'role'=>$request->role
-    ]);
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'telephone' => $request->telephone,
+            'role' => $request->role
+        ]);
         return response()->json([
-            "massage"=>'Compte enregistré avec succès',
-            "data"=>$user
+            "massage" => 'Compte enregistré avec succès',
+            "data" => $user
 
-        ],201);
+        ], 201);
 
     }
 
--- sharayan-backend/routes/api.php
+++ sharayan-backend/routes/api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\Authcontroller;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [Authcontroller::class, 'register']);
+Route::post('/login', [Authcontroller::class, 'login']);
